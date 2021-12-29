cf1201a6357a71b06de790b165ca0c13b1801373
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2021-12-29 22:19:16 +0530

    v1 of app

---
+++ script.js
@@ -0,0 +1,140 @@
+const addForm = document.querySelector('.form-add')
+const editForm = document.querySelector('.form-edit')
+const taskContainer = document.querySelector('.task-container')
+const taskInput = document.getElementById('add-task')
+const editInput = document.getElementById('edit-task')
+const editButton = editForm.querySelector('button[type="submit"]')
+
+// Initial state variables
+let id
+let taskStore = []
+let taskToBeEditedID
+
+// Get the saved tasks on page load
+getLocalStorage()
+
+// Create a task and display it on UI
+addForm.addEventListener('submit', e => {
+  e.preventDefault()
+  // Take the input value and store in a variable
+  const inputValue = taskInput.value
+  // Check if user has entered something
+  if (inputValue === '') return alert('Please, Enter a valid task.....')
+  // Check what the id of last added task and add 1 and if there's no task set the id as 1
+  id = taskStore.at(-1)?.id ? taskStore.at(-1).id + 1 : 1
+  // Push the object of task to taskStore
+  taskStore.push({id: id, value: inputValue})
+  id++
+  // Display the tasks on UI
+  displayTasks(taskStore)
+  // Store the taskStore in local storage
+  setLocalStorage(taskStore)
+  // Clear the inputs
+  taskInput.value = ''
+})
+
+// Edit the task and re-render the UI
+editForm.addEventListener('submit', e => {
+  e.preventDefault()
+  // Get the task that needs to be edited
+  const task = taskStore.find(task => task.id === taskToBeEditedID)
+  // Set the value of task as the edited one
+  task.value = editInput.value
+  // Display tasks to UI
+  displayTasks(taskStore)
+  // Save the tasks to local storage
+  setLocalStorage(taskStore)
+  // Clear the Input field
+  editInput.value = ''
+  editInput.disabled = true
+  editButton.disabled = true
+})
+
+// Function that takes an array and display html
+function displayTasks(arr) {
+  // Clear the exisiting sample html
+  taskContainer.innerHTML = ''
+  // Loop over the array of tasks and create a html template
+  arr.forEach(task => {
+    const html = `
+      <li id="${task.id}">
+        <p>${task.value}</p>
+        <div class="task-actions">
+          <Button class="btn-edit">Edit</Button>
+          <Button class="btn-delete">Delete</Button>
+        </div>
+      </li>
+    `
+    // Insert the html in the element
+    taskContainer.insertAdjacentHTML('afterbegin', html)
+
+    // Attatch the event listeners to buttons
+    const deleteBtns = document.querySelectorAll('.btn-delete')
+    const editBtns = document.querySelectorAll('.btn-edit')
+    const checkTask = document.querySelectorAll('input[type="checkbox"]')
+    deleteBtns.forEach(btn => btn.addEventListener('click', deleteTask))
+    editBtns.forEach(btn => btn.addEventListener('click', editTask))
+  })
+}
+
+// Function that saves the taskStore to local storage
+function setLocalStorage(arr) {
+  localStorage.setItem('tasks', JSON.stringify(arr))
+}
+
+// Function that takes from local storage and display it on UI
+function getLocalStorage() {
+  // Get the parsed data from local storage
+  const data = JSON.parse(localStorage.getItem('tasks'))
+  // if no tasks return
+  if (!data) return (taskContainer.innerHTML = '<p>No Tasks Available!</p>')
+  // Set the taskStore as data
+  taskStore = data
+  // Display the tasks to UI
+  displayTasks(taskStore)
+}
+
+// Function that resets local storage
+function resetLocalStoarge() {
+  // if no tasks reset the storage and display the html
+  if (!taskStore.length) {
+    localStorage.removeItem('tasks')
+    taskContainer.innerHTML = '<p>No Tasks Available!</p>'
+  }
+}
+
+// Function that deletes the tasks
+function deleteTask(e) {
+  // Get the task element that is to be deleted
+  const taskToBeDeletedEl = e.target.closest('li')
+  // Get the index of the task
+  const taskIndex = taskStore.findIndex(
+    task => task.id === +taskToBeDeletedEl.id,
+  )
+  // Remove the task from the task store
+  taskStore.splice(taskIndex, 1)
+  // Set the local storage
+  setLocalStorage(taskStore)
+  // Display the tasks to UI
+  displayTasks(taskStore)
+  // Reset local storage if no tasks
+  resetLocalStoarge()
+}
+
+// Function that edits the tasks
+function editTask(e) {
+  // Get the task element to be edited
+  const taskToBeEditedEl = e.target.closest('li')
+  // Get the task value from task store
+  const taskToBeEdited = taskStore.find(
+    task => task.id === +taskToBeEditedEl.id,
+  )
+  // Enable edit input and button
+  editInput.disabled = false
+  editButton.disabled = false
+  // Set the input value of edit as the task value
+  editInput.value = taskToBeEdited.value
+  editInput.focus()
+  taskToBeEditedID = taskToBeEdited.id
+  return taskToBeEditedID
+}
